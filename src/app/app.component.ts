4196ea626142c7a29b493e31fbeaf64962f52fbf
celoazevedo <meazevedo86@gmail.com>
2020-05-04 13:19:44 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /* We can interpolate or bind data to our template from our class properties of our app component
+  */
+
+  // PROPERTIES
+
   name = 'Angular';
 
   isTrue = false;
@@ -13,11 +18,48 @@ export class AppComponent {
   timesSkydive = 3
   subtract = function () { 30 - 7 }
   favFruit = 'apple'
-  jason = { firstName: 'Jason', lastName: 'Bourne', age: 22, favoriteTV: 'Simpsons', isWorking: false }
+  jason = { firstName: 'Jason', lastName: 'Bourne', age: 22,favoriteTV: 'Simpsons', isWorking: false }
   family = ['mom', 'dad', 'sister', 'brother', 'dog']
   url = 'https://www.google.com'
   linkText = 'Click Me'
-  color = 'blue';
+  // color = 'blue';
+
+  // METHODS
+  clickAlert() {
+    console.log("This was clicked!");
+    alert("Hello! You clicked THE button. ")
+  }
+
+  changeColor() {
+    console.log("you just changed the text color! and the favFruit!")
+    this.favFruit = "banana";
+    this.name = "super Angular";
+  }
+
+//figure out how to better interpolate Arrays. 
+  // clickAlert1() {
+  // console.log(this.jason.firstName);
+  // alert(this.jason.firstName);
+  // }
 
 
 }
+
+
+// METHODS EXAMPLES
+
+//   clickAlert() {
+//     console.log("This was clicked!");
+//     alert("Hello! You clicked a button. ")
+//   }
+
+//   changeColor() {
+//     console.log("The color was changed!")
+//     this.color = "red";
+//     this.title = "New Title";
+//   }
+
+//   returnValue() {
+//     let val = "Return Value";
+//     return val;
+//   }
